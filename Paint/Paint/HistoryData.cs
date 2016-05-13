ccf2926e6defc36ad7073c39c3c582400e633708
Valeriy Karnaukhov <Valera-96@yandex.ru>
2016-05-13 17:40:16 +0300

    Added some comments

--- Paint/Paint/HistoryData.cs
+++ Paint/Paint/HistoryData.cs
@@ -11,10 +11,30 @@ namespace Paint
     /// </summary>
     public class HistoryData
     {
+        /// <summary>
+        /// Предоставляет список фигур, нарисованных на графическом окне
+        /// </summary>
         public List<Shape> Shapes { get; set; }
+
+        /// <summary>
+        /// Предоставляет коллекцию объектов Bitmap, верхний элемент которой является главным изображением 
+        /// на графическом окне в данный момент
+        /// </summary>
         public Stack<System.Drawing.Bitmap> Bitmaps { get; set; }
+
+        /// <summary>
+        /// Предоставляет коллекцию объектов, содержащую сведения о размерах панели, которые менялись в ходе работы
+        /// </summary>
         public Stack<PanelSize> PanelSizes { get; set; }
+
+        /// <summary>
+        /// Предоставляет коллецию объектов, изображения которых в ходе работы были внутри области выделения
+        /// </summary>
         public Stack<MyBitmap> RegionBitmaps { get; set; }
+
+        /// <summary>
+        /// Предоставляет коллекцию объектов, которая в качестве своих элементов содержит параметры текста
+        /// </summary>
         public Stack<TextSettings> TextSettings { get; set; }
 
         public HistoryData()
